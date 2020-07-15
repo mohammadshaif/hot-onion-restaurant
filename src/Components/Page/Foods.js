3265b5715fe851434b2fd4fbb09c7fec67e4ef20
mohammadshaif <mohammadeshaif27@gmail.com>
2020-07-15 23:48:52 +0600

    s

--- src/Components/Page/Foods.js
+++ src/Components/Page/Foods.js
@@ -1,24 +1,30 @@
 import React, { useState, useEffect } from 'react';
 //import foods from '../../fakeData/foodData';
 import FoodItem from '../Food/FoodItem';
+import Loading from '../Food/Loading';
 
 const Foods = () => {
   const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(()=>{
+    setIsLoading(true);
     fetch('https://secret-tundra-37169.herokuapp.com/foods/')
     .then(response => response.json())
     .then(data => {
       setItems(data)
+      setIsLoading(false)
     })
 },[])
-
+if (isLoading) {
+  return <Loading />
+}
   // useEffect(() => {
   //     setItems(foods)
   // }, [])
   return (
     <div className="container">
       <div className="row food-items">
-      {items.map(item => <FoodItem key={item.id} item={item} />)}
+      {items.map(item => <FoodItem key={item.id} food={item} />)}
     </div>
     </div>
   );
